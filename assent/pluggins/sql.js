601164b5d440d08eb65309b068fe16a2a331894d
KING_ROMEO <103659324+JavisSL@users.noreply.github.com>
2022-04-15 16:08:34 +0530

    Update and rename sql to sql.js

--- assent/pluggins/sql.js
+++ assent/pluggins/sql.js
@@ -7,3 +7,6 @@ Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
 */
 
+const KingRomeo = require('kingromeo-public');
+const Build = QueenAmdi.build
+const { DataTypes } = require('@javissl/sequelize');
