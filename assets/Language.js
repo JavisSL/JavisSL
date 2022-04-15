2a909ea8f37efe94d7fae2483dd5ca5731e0c02e
KING_ROMEO <103659324+JavisSL@users.noreply.github.com>
2022-04-15 18:18:57 +0530

    Update Language.js

--- assets/Language.js
+++ assets/Language.js
@@ -5,6 +5,7 @@
 Copyright (C) 2021 JavisSL. 
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
+*/
 
 const KingRomeo = require('kingromeo-public');
 const build = KingRomeo.build
