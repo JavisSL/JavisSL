a565ce074a96eb8dfd061f5780a0090292321c51
KING_ROMEO <103659324+JavisSL@users.noreply.github.com>
2022-04-15 19:58:30 +0530

    Update qr.js

--- qr.js
+++ qr.js
@@ -2,10 +2,10 @@
 █▀█ █▀▀█ █▀█ ▄█─ 
  ─▄▀ █▄▀█ ─▄▀ ─█─ 
 ╚█▄▄ █▄▄█ █▄▄ ▄█▄
-*/
 Copyright (C) 2021 JavisSL. 
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
+*/
 
 const chalk = require('chalk');
 const { WAConnection, MessageType } = require('@javissl/javissl-web-api');
